d71b1de5683e3145d6f8bc50a00a6febde7863b4
Deejay Geroso <deejaygeroso@gmail.com>
2021-08-21 19:32:14 +0800

    Configured express v4 implementation of bodyParser

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,9 @@ import { processENV } from './constants'
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use('/api', apiRoute)
 app.get('/', (req: Request, res: Response) => res.send('Express + TypeScript Server'))
 
